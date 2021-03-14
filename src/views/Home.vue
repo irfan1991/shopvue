65284aae6a61a64a62cfc64dbbb95d5e125db736
irfan1991 <irfanprasetyo91@gmail.com>
2021-03-14 21:27:13 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+      <Header />
+       <Hero />
+       <Product />
+       <Instagram />
+       <Partner />
+       <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue';
+import Hero from '@/components/Hero.vue';
+import Product from '@/components/Product.vue';
+import Instagram from '@/components/Instagram.vue';
+import Partner from '@/components/Partner.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    Product,
+    Instagram,
+    Partner,
+    Footer
+    
+  }
+}
+    Footer
+</script>
